4fb3e892c9e93e37efeee112c6f0cebf34622410
Resül Çay <resulcayop@gmail.com>
2023-08-15 15:07:59 +0300

    const modifiers added

--- lib/components/wild_drop_widget.dart
+++ lib/components/wild_drop_widget.dart
@@ -9,8 +9,8 @@ class WildDropWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
+    return const Column(
+      children: [
         DropDownComponent(),
         ColorSection(),
       ],
